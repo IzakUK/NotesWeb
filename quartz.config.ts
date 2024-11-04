e7628fb491a8cbb3e2ced8481681fc88560f1116
IzakUK <izakf1738@gmail.com>
2024-11-04 18:44:20 +0000

    Quartz sync: Nov 4, 2024, 6:44 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Go Back To Home",
+    pageTitle: "Izak.UK",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
